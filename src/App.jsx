c1ba2784974309715edd155031f8b67a197adcd5
atenziono <atenziono@gmail.com>
2024-11-26 17:03:12 +0330

    about secntion and navbar finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import About from "./components/About";
+import Features from "./components/Features";
 import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <NavBar />
       <Hero />
       <About />
+      <Features />
     </main>
   );
 };
---
+++ src/components/Features.jsx
@@ -0,0 +1,5 @@
+const Features = () => {
+  return <section className="bg-black pb-52">Features</section>;
+};
+
+export default Features;
